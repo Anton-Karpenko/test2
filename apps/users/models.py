85f3ff77674c8ca2533c3bfa00f2596ac2d032cc
Anton Karpenko <antykkarp@gmail.com>
2019-06-06 15:21:35 +0300

    Stylistic improvement for user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -6,7 +6,11 @@ from django.utils.translation import ugettext_lazy as _
 class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(
+        _("Name of User"),
+        blank=True,
+        max_length=255
+    )
 
     class Meta:
         db_table = 'users'
