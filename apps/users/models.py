16ef0a7c2cf9553e543ea0204e5c2261f2737a14
Anton Karpenko <antykkarp@gmail.com>
2019-06-06 14:20:18 +0300

    Initial commit

---
+++ apps/users/models.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class User(AbstractUser):
+    # First Name and Last Name do not cover name patterns
+    # around the globe.
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+
+    class Meta:
+        db_table = 'users'
